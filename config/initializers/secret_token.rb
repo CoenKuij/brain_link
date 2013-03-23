3d68ed617cf9eb528c053ccf96db17c8614c7b73
Coen Kuijpers <coenehv@gmail.com>
2013-03-23 14:58:35 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BrainLink::Application.config.secret_token = '48e32ea68da73e0c0e3a820f952b9ca8c62caf6940df69d028519bda009bd292d5abc3dfcae47dd77aec041becfe525d10cf732cd2175ad4b9c309aa82592f89'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+BrainLink::Application.config.session_store :cookie_store, key: '_brain_link_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# BrainLink::Application.config.session_store :active_record_store
